2637bae165bb8dcab6147c2fd514d09adef6a200
oscarm524 <oscar.aguilar.1987@gmail.com>
2026-03-12 06:50:54 -0500

    adding another xgboost script

---
+++ 2026/PS_Ep3/xgb_3.py
@@ -0,0 +1,90 @@
+import warnings
+
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import StratifiedKFold
+
+warnings.filterwarnings("ignore")
+
+df = pd.read_csv("train.csv", index_col="id")
+df["SeniorCitizen"] = df["SeniorCitizen"].astype("category")
+df["Churn"] = df["Churn"].map({"No": 0, "Yes": 1})
+
+test = pd.read_csv("test.csv", index_col="id")
+test["SeniorCitizen"] = test["SeniorCitizen"].astype("category")
+
+original = pd.read_csv(
+    "WA_Fn-UseC_-Telco-Customer-Churn.csv",
+    index_col="customerID"
+)
+original["SeniorCitizen"] = original["SeniorCitizen"].astype("category")
+original["Churn"] = original["Churn"].map({"No": 0, "Yes": 1})
+
+# Convert object columns with 3 or more unique labels to category dtype
+object_cols = df.select_dtypes(include="object").columns.to_list()
+# Exclude target variable if it exists in object columns
+if "Churn" in object_cols:
+    object_cols.remove("Churn")
+
+for col in object_cols:
+    df[col] = df[col].astype("category")
+    test[col] = test[col].astype("category")
+
+X = df.drop(columns=["Churn"])
+y = df["Churn"]
+
+# Create DMatrix for test data
+dtest = xgb.DMatrix(test, enable_categorical=True)
+
+xgb_params = {
+    "objective": "binary:logistic",
+    "eval_metric": "auc",
+    "learning_rate": 0.15,
+    "max_depth": 2,
+    "reg_lambda": 10,
+    "max_bin": 20000,
+    "device": "cuda",
+    "random_state": 42,
+    "verbose": -1,
+    "n_jobs": -1,
+}
+
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+cat_features = X.select_dtypes(include="category").columns.tolist()
+
+aucs, test_preds = [], []
+for fold, (train_index, valid_index) in enumerate(skf.split(X, y)):
+
+    X_train, X_valid = X.iloc[train_index], X.iloc[valid_index]
+    y_train, y_valid = y.iloc[train_index], y.iloc[valid_index]
+
+    dtrain = xgb.DMatrix(X_train, label=y_train, enable_categorical=True)
+    dvalid = xgb.DMatrix(X_valid, label=y_valid, enable_categorical=True)
+
+    model = xgb.train(
+        xgb_params,
+        dtrain,
+        num_boost_round=10000,
+        evals=[(dvalid, "validation")],
+        early_stopping_rounds=300,
+        verbose_eval=False,
+    )
+
+    y_pred = model.predict(dvalid)
+    auc = roc_auc_score(y_valid, y_pred)
+    aucs.append(auc)
+    print(f"Fold {fold + 1} AUC: {auc:.4f}")
+
+    test_pred = model.predict(dtest)
+    test_preds.append(test_pred)
+
+print(f"Mean AUC: {np.mean(aucs):.4f}")
+
+# Fold 1 AUC: 0.9182
+# Fold 2 AUC: 0.9188
+# Fold 3 AUC: 0.9181
+# Fold 4 AUC: 0.9192
+# Fold 5 AUC: 0.9166
+# Mean AUC: 0.9182
